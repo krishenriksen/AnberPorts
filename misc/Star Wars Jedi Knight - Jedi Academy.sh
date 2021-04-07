e51d3d8031a23a6ecf2ca3c0f1756e11da914a22
krishenriksen <github@krishenriksen.dk>
2021-04-07 11:51:19 +0700

    fixed path

--- misc/Star Wars Jedi Knight - Jedi Academy.sh
+++ misc/Star Wars Jedi Knight - Jedi Academy.sh
@@ -8,18 +8,18 @@ export LIBGL_FB=4
 
 # system
 export LD_LIBRARY_PATH=$GAMEDIR/lib:$LIBDIR:/usr/lib32:/usr/local/lib/arm-linux-gnueabihf/
-export LD_PRELOAD=$GAMEDIR/lib/libGL.so.1
+export LD_PRELOAD=$LIBDIR/libGL.so.1
 
 cd $GAMEDIR
 
 if id "ark" &>/dev/null || id "odroid" &>/dev/null; then
-  sudo $BINDIR/oga_controls openjk_sp &
+  sudo $BINDIR/oga_controls openjk_sp.arm	&
 else
   patchelf --set-interpreter /usr/lib32/ld-linux-armhf.so.3 $GAMEDIR/openjk_sp.arm
-  $BINDIR/oga_controls openjk_sp &
+  $BINDIR/oga_controls openjk_sp.arm &
 fi
 
-./openjk_sp.arm 2>&1 | tee -a ./log.txt
+./openjk_sp.arm -nosplash 2>&1 | tee -a $GAMEDIR/log.txt
 
 # make sure AnberPorts-Joystick is killed if quit via game menu
 if id "ark" &>/dev/null || id "odroid" &>/dev/null; then
