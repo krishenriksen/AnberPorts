48cf629a05026cd88107d99c040af9b3210c4a9c
krishenriksen <github@krishenriksen.dk>
2021-04-09 06:36:08 +0700

    downgraded to v1.1

--- misc/AM2R.sh
+++ misc/AM2R.sh
@@ -7,7 +7,7 @@ BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/AnberP
 export LIBGL_FB=4
 
 # system
-export LD_LIBRARY_PATH=$LIBDIR:/usr/lib32:/usr/local/lib/arm-linux-gnueabihf/
+export LD_LIBRARY_PATH=$LIBDIR:/usr/lib32
 
 # box86
 export BOX86_ALLOWMISSINGLIBS=1
@@ -18,17 +18,14 @@ export BOX86_PATH=$BINDIR
 cd $GAMEDIR
 
 if id "ark" &>/dev/null || id "odroid" &>/dev/null; then
-  sudo $BINDIR/oga_controls box86 &
-else
-  patchelf --set-interpreter /usr/lib32/ld-linux-armhf.so.3 $BINDIR/box86
-  $BINDIR/oga_controls box86 &
-fi
-
-$BINDIR/box86 ./runner 2>&1 | tee -a ./log.txt
-
-# make sure AnberPorts-Joystick is killed if quit via game menu
-if id "ark" &>/dev/null || id "odroid" &>/dev/null; then
+  setarch linux32 -L $BINDIR/box86 ./am2r 2>&1 | tee -a ./log.txt &
+  sudo $BINDIR/oga_controls
   pgrep -f oga_controls | sudo xargs kill -9
 else
+  patchelf --set-interpreter /usr/lib32/ld-linux-armhf.so.3 $BINDIR/box86
+  $BINDIR/box86 ./am2r 2>&1 | tee -a ./log.txt &
+  $BINDIR/oga_controls
   pgrep -f oga_controls | xargs kill -9
 fi
+
+pgrep -f box86 | xargs kill -9
