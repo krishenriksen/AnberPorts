0dbe7ac4b51c4b7049cb0393e9eda9f5d93a533f
krishenriksen <github@krishenriksen.dk>
2021-04-10 18:16:50 +0700

    updated for 351elec

--- misc/Diablo 1.sh
+++ misc/Diablo 1.sh
@@ -1,5 +1,27 @@
-#/bin/bash
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/diablo1"
-export SDL_GAMECONTROLLERCONFIG="$(cat gamecontrollerdb.txt)"
+#!/bin/bash
+GAMEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/diablo1"
+LIBDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/AnberPorts/lib64"
+BINDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/AnberPorts/bin"
+
+# gl4es
+export LIBGL_FB=4
+
+# system
+export LD_LIBRARY_PATH=$LIBDIR:/usr/lib
+
+cd $GAMEDIR
+
+if id "ark" &>/dev/null || id "odroid" &>/dev/null; then
+  sudo $BINDIR/oga_controls devilutionx &
+else
+  $BINDIR/oga_controls devilutionx &
+fi
+
 ./devilutionx 2>&1 | tee -a ./log.txt
-unset SDL_GAMECONTROLLERCONFIG
+
+# make sure AnberPorts-Joystick is killed if quit via game menu
+if id "ark" &>/dev/null || id "odroid" &>/dev/null; then
+  pgrep -f oga_controls | sudo xargs kill -9
+else
+  pgrep -f oga_controls | xargs kill -9
+fi
